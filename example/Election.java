ecd2074fa7a1fc13d0b40ff5d796767b7281133e
Laricheva10 <valerial10@yandex.ru>
2026-01-14 04:09:51 +0300

    Create Election.java

---
+++ example/Election.java
@@ -0,0 +1,39 @@
+package labs;
+
+public class Candidate {
+    private String name;
+    private int votes;
+
+    public Candidate(String name) {
+        this.name = name;
+        this.votes = 0;
+ 
+    public Candidate(String name, int votes) {
+        this.name = name;
+        this.votes = votes;
+   
+    public String getName() {
+        return name;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+
+    public void addVote() {
+        votes++;
+    }
+
+    public void addVotes(int n) {
+        votes += n;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + votes + " голосов";
+    }
+}
